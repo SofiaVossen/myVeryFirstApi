1d4189bcdf0fca58e250f8d580429d0e2540117d
SofiaVossen <sofiavossen@gmail.com>
2022-12-01 21:25:27 +0100

    new

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,26 @@
-console.log(Math.PI);
+//1.variable benennen und mit elementbid holen
+    let container = document.getElementById("container");
 
-const PI=PI;
-const roundedPI=
+//2. Api fetchen
+fetch("https://picsum.photos/v2/list")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+
+//3.foreach methode anwenden
+        data.forEach(block => {
+    //3.1.html tags kreieren mit .createElement
+            let blockImg = document.createElement("img");
+            let blockText = document.createElement("figcaption");
+            let blockFigure = document.createElement("figure");
+    //3.2. den Inhalt von blockImg und blockText festlegen durch holen der propertys aus dem array
+            blockImg.src = block.download_url;
+            blockText.textContent = block.author;
+    //3.3. bei var blockFigure: 2 childs anhängen jeweils image und text 
+            blockFigure.appendChild(blockImg);
+            blockFigure.appendChild(blockText);
+    //3.4. bei id container: als child die figure anhängen
+            container.appendChild(blockFigure);
+        });
+        
+    });
